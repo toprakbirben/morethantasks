37f3a50a33c944c0779d01f5d169678530f4153f
Toprak <s.t.birben@student.vu.nl>
2025-04-29 12:30:05 +0200

    Initial Commit

---
+++ morethantasks/morethantasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  morethantasksApp.swift
+//  morethantasks
+//
+//  Created by Toprak Birben on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct morethantasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ morethantasksTests/morethantasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  morethantasksTests.swift
+//  morethantasksTests
+//
+//  Created by Toprak Birben on 29/04/2025.
+//
+
+import Testing
+@testable import morethantasks
+
+struct morethantasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
